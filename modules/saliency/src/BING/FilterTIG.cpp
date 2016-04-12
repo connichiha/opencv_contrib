807dc8d63f2049ad0456c05a1f1fda7325c08ae2
connichiba <wanghao.bit@gmail.com>
2016-04-12 22:15:31 +0900

    fix the error of INT64 Mat
    OpenCV Error: Assertion failed <udata<<uchar*>ptr
    &&<<uchar*>ptr-udata><=.... in cv::fastFree, file ****src\alloc.cpp,
    line  78

--- modules/saliency/src/BING/FilterTIG.cpp
+++ modules/saliency/src/BING/FilterTIG.cpp
@@ -39,7 +39,8 @@
  //
  //M*/
 
-#include "precomp.hpp"
+//#include "BING/saliencyClasses.h"
+#include "saliencyClasses.h"
 #include "CmShow.hpp"
 
 namespace cv
@@ -107,49 +108,59 @@ void ObjectnessBING::FilterTIG::reconstruct( Mat &w1f )
 
 // For a W by H gradient magnitude map, find a W-7 by H-7 CV_32F matching score map
 // Please refer to my paper for definition of the variables used in this function
-Mat ObjectnessBING::FilterTIG::matchTemplate( const Mat &mag1u )
+void ObjectnessBING::FilterTIG::matchTemplate(const Mat &mag1u, Mat &matchCost1f)
 {
-  const int H = mag1u.rows, W = mag1u.cols;
-  const Size sz( W + 1, H + 1 );  // Expand original size to avoid dealing with boundary conditions
-  Mat_<int64_t> Tig1 = Mat_<int64_t>::zeros( sz ), Tig2 = Mat_<int64_t>::zeros( sz );
-  Mat_<int64_t> Tig4 = Mat_<int64_t>::zeros( sz ), Tig8 = Mat_<int64_t>::zeros( sz );
-  Mat_<BYTE> Row1 = Mat_<BYTE>::zeros( sz ), Row2 = Mat_<BYTE>::zeros( sz );
-  Mat_<BYTE> Row4 = Mat_<BYTE>::zeros( sz ), Row8 = Mat_<BYTE>::zeros( sz );
-  Mat_<float> scores( sz );
-  for ( int y = 1; y <= H; y++ )
-  {
-    const BYTE* G = mag1u.ptr<BYTE>( y - 1 );
-    int64_t* T1 = Tig1.ptr<int64_t>( y );  // Binary TIG of current row
-    int64_t* T2 = Tig2.ptr<int64_t>( y );
-    int64_t* T4 = Tig4.ptr<int64_t>( y );
-    int64_t* T8 = Tig8.ptr<int64_t>( y );
-    int64_t* Tu1 = Tig1.ptr<int64_t>( y - 1 );  // Binary TIG of upper row
-    int64_t* Tu2 = Tig2.ptr<int64_t>( y - 1 );
-    int64_t* Tu4 = Tig4.ptr<int64_t>( y - 1 );
-    int64_t* Tu8 = Tig8.ptr<int64_t>( y - 1 );
-    BYTE* R1 = Row1.ptr<BYTE>( y );
-    BYTE* R2 = Row2.ptr<BYTE>( y );
-    BYTE* R4 = Row4.ptr<BYTE>( y );
-    BYTE* R8 = Row8.ptr<BYTE>( y );
-    float *s = scores.ptr<float>( y );
-    for ( int x = 1; x <= W; x++ )
-    {
-      BYTE g = G[x - 1];
-      R1[x] = ( R1[x - 1] << 1 ) | ( ( g >> 4 ) & 1 );
-      R2[x] = ( R2[x - 1] << 1 ) | ( ( g >> 5 ) & 1 );
-      R4[x] = ( R4[x - 1] << 1 ) | ( ( g >> 6 ) & 1 );
-      R8[x] = ( R8[x - 1] << 1 ) | ( ( g >> 7 ) & 1 );
-      T1[x] = ( Tu1[x] << 8 ) | R1[x];
-      T2[x] = ( Tu2[x] << 8 ) | R2[x];
-      T4[x] = ( Tu4[x] << 8 ) | R4[x];
-      T8[x] = ( Tu8[x] << 8 ) | R8[x];
-      s[x] = dot( T1[x], T2[x], T4[x], T8[x] );
-    }
-  }
-  Mat matchCost1f;
-  scores( Rect( 8, 8, W - 7, H - 7 ) ).copyTo( matchCost1f );
-  return matchCost1f;
-}
+	const int H = mag1u.rows, W = mag1u.cols;
+	const Size sz(W + 1, H + 1); // Expand original size to avoid dealing with boundary conditions  
+	Mat_<float> scores(sz);
+	const int sizeSZ = sz.width * sz.height;
+	INT64 * Tig1 = (INT64 *)malloc(sizeSZ * sizeof(INT64));
+	INT64 * Tig2 = (INT64 *)malloc(sizeSZ * sizeof(INT64));
+	INT64 * Tig4 = (INT64 *)malloc(sizeSZ * sizeof(INT64));
+	INT64 * Tig8 = (INT64 *)malloc(sizeSZ * sizeof(INT64));
+	byte * Row1 = (byte *)malloc(sizeSZ * sizeof(byte));
+	byte * Row2 = (byte *)malloc(sizeSZ * sizeof(byte));
+	byte * Row4 = (byte *)malloc(sizeSZ * sizeof(byte));
+	byte * Row8 = (byte *)malloc(sizeSZ * sizeof(byte));
+	memset(Tig1, 0, sizeSZ * sizeof(INT64)); memset(Tig2, 0, sizeSZ * sizeof(INT64));
+	memset(Tig4, 0, sizeSZ * sizeof(INT64)); memset(Tig8, 0, sizeSZ * sizeof(INT64));
+	memset(Row1, 0, sizeSZ * sizeof(byte)); memset(Row2, 0, sizeSZ * sizeof(byte));
+	memset(Row4, 0, sizeSZ * sizeof(byte)); memset(Row8, 0, sizeSZ * sizeof(byte));
+	for (int y = 1; y <= H; y++)
+	{
+		const byte * G = mag1u.ptr(y - 1);
+		INT64 * T1 = Tig1 + y*sz.width;
+		INT64 * T2 = Tig2 + y*sz.width;
+		INT64 * T4 = Tig4 + y*sz.width;
+		INT64 * T8 = Tig8 + y*sz.width;
+		INT64 * Tu1 = Tig1 + (y - 1)*sz.width;
+		INT64 * Tu2 = Tig2 + (y - 1)*sz.width;
+		INT64 * Tu4 = Tig4 + (y - 1)*sz.width;
+		INT64 * Tu8 = Tig8 + (y - 1)*sz.width;
+		byte * R1 = Row1 + y*sz.width;
+		byte * R2 = Row2 + y*sz.width;
+		byte * R4 = Row4 + y*sz.width;
+		byte * R8 = Row8 + y*sz.width;
+		float *s = scores.ptr<float>(y);
+		for (int x = 1; x <= W; x++)
+		{
+			byte g = G[x - 1];
+			R1[x] = (R1[x - 1] << 1) | ((g >> 4) & 1);
+			R2[x] = (R2[x - 1] << 1) | ((g >> 5) & 1);
+			R4[x] = (R4[x - 1] << 1) | ((g >> 6) & 1);
+			R8[x] = (R8[x - 1] << 1) | ((g >> 7) & 1);
+			T1[x] = (Tu1[x] << 8) | R1[x];
+			T2[x] = (Tu2[x] << 8) | R2[x];
+			T4[x] = (Tu4[x] << 8) | R4[x];
+			T8[x] = (Tu8[x] << 8) | R8[x];
+			s[x] = dot(T1[x], T2[x], T4[x], T8[x]);
+		}
+	}
+	free(Tig1); free(Tig2); free(Tig4); free(Tig8);
+	free(Row1); free(Row2); free(Row4); free(Row8);
 
+	scores(Rect(8, 8, W - 7, H - 7)).copyTo(matchCost1f);
+
+}
 }  // namespace saliency
 }  // namespace cv
